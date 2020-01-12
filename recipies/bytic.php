29e92d5975a5267e3d527d5de35ab968409aeacc
Gabriel Solomon <hello@gabrielsolomon.ro>
2020-01-12 18:53:49 +0200

    update merge

--- recipies/bytic.php
+++ recipies/bytic.php
@@ -1,41 +1,26 @@
 <?php
 
-<<<<<<< HEAD
 /* (c) Gabriel Solomon <hello@gabrielsolomon.ro>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
-=======
-/*
->>>>>>> 4.0
  * This recipe supports ByTic Framework 0.9
  */
 
 namespace Deployer;
 
-<<<<<<< HEAD
 require 'recipe/common.php';
 require 'recipe/npm.php';
 require 'recipe/cloudflare.php';
 
 require_once __DIR__ . '/git-submodules.php';
+require_once __DIR__.'/bytic-console.php';
 
 /*** CONFIGURATION ***/
 set('ssh_type', 'native');
 set('ssh_multiplexing', true);
-//set('git_cache', true);
-=======
-require 'vendor/deployer/deployer/recipe/common.php';
-require 'vendor/deployer/recipes/npm.php';
-require_once __DIR__.'/git-submodules.php';
-require_once __DIR__.'/bytic-console.php';
-
-/*** CONFIGURATION ***/
-//set('ssh_type', 'native');
-//set('ssh_multiplexing', true);
 set('git_cache', true);
->>>>>>> 4.0
 
 set('keep_releases', 3);
 //set('composer_command', 'composer'); // Path to composer
