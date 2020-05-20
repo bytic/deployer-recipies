ce121e8ecefe8552dcfae7f1bf63db664655ef93
Gabriel Solomon <hello@gabrielsolomon.ro>
2020-05-20 11:26:50 +0300

    move config to separate files

--- recipies/bytic.php
+++ recipies/bytic.php
@@ -8,43 +8,13 @@ namespace Deployer;
 
 require 'vendor/deployer/deployer/recipe/common.php';
 
+require_once __DIR__ . '/bytic-config.php';
+
 require_once __DIR__.'/npm.php';
 require_once __DIR__.'/git-submodules.php';
 require_once __DIR__.'/bytic-commands.php';
 require_once __DIR__.'/bytic-console.php';
 
-/*** CONFIGURATION ***/
-//set('ssh_type', 'native');
-//set('ssh_multiplexing', true);
-set('git_cache', true);
-
-
-set('keep_releases', 3);
-//set('composer_command', 'composer'); // Path to composer
-set('writable_use_sudo', false); // Using sudo in writable commands?
-
-//env('composer_options',
-//    'install --no-dev --verbose --prefer-dist --optimize-autoloader --no-progress --no-interaction');
-set('release_name', date('YmdHis')); // name of folder in releases
-
-/*** SHARED FILES ***/
-set('shared_files', [
-    '.env',
-]);
-
-/*** SHARED DIRS ***/
-set('shared_dirs', [
-    'storage/app',
-    'storage/logs',
-]);
-
-/*** WRITABLES DIRS ***/
-set('writable_dirs', [
-    'storage/app',
-    'storage/logs',
-    'storage/cache',
-    'storage/cache/autoloader',
-]);
 
 /*** MAIN TASK ***/
 desc('Deploy your project');
@@ -70,7 +40,7 @@ task('deploy', [
  * Helper tasks
  */
 desc('Optimize deployed application');
-task('artisan:up', ['bytic:optimize']);
+task('deploy:optimize', ['bytic:optimize']);
 
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
