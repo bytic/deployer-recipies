c4b6e0df544e219f0801ad9d8cd1aab9b44c2a74
Gabriel Solomon <hello@gabrielsolomon.ro>
2021-04-25 14:36:23 +0300

    add cleaup vendors and node

--- recipies/bytic-commands.php
+++ recipies/bytic-commands.php
@@ -2,11 +2,50 @@
 
 namespace Deployer;
 
+task(
+    'deploy:storage-symlink',
+    function () {
+        run("cd {{deploy_path}} && {{bin/symlink}} {{release_path}}/storage/app/public current/public/uploads ");
+    }
+);
+
 desc('Optimize Bytic');
-task('bytic:optimize', function () {
-    cd('{{release_path}}');
+task(
+    'bytic:optimize',
+    function () {
+        cd('{{release_path}}');
 
-    $byticCmd = get('bytic_get_cmd')('config:cache', []);
+        $byticCmd = get('bytic_get_cmd')('config:cache', []);
 
-    run($byticCmd);
-});
+        run($byticCmd);
+    }
+);
+
+task(
+    'bytic:previous_release:cleanup_vendors',
+    function () {
+        if (!has('previous_release')) {
+            return;
+        }
+        if (!test('[ -d {{previous_release}}/vendor ]')) {
+            return;
+        }
+        $sudo = get('cleanup_use_sudo') ? 'sudo' : '';
+        run("$sudo rm -rf {{previous_release}}/vendor");
+    }
+);
+
+task(
+    'bytic:previous_release:cleanup_cache',
+    function () {
+        if (!has('previous_release')) {
+            return;
+        }
+        if (!test('[ -d {{previous_release}}/vendor ]')) {
+            return;
+        }
+//        $sudo = get('cleanup_use_sudo') ? 'sudo' : '';
+//        run("$sudo rm -rf {{previous_release}}/vendor");
+    }
+);
+
--- recipies/bytic.php
+++ recipies/bytic.php
@@ -4,6 +4,7 @@ namespace Deployer;
 
 require 'recipe/common.php';
 require __DIR__ . '/../common/cloudflare.php';
+require __DIR__ . '/../common/node_modules.php';
 require __DIR__ . '/../common/npm.php';
 
 require_once __DIR__ . '/bytic-config.php';
@@ -13,12 +14,6 @@ require_once __DIR__ . '/bytic-console.php';
 
 /*** DEFINE TASKS ***/
 task(
-    'deploy:storage-symlink',
-    function () {
-        run("cd {{deploy_path}} && {{bin/symlink}} {{release_path}}/storage/app/public current/public/uploads ");
-});
-
-task(
     'deploy:git-cache',
     function () {
         run('git config --global core.compression 0');
@@ -27,44 +22,43 @@ task(
 
 /*** MAIN TASK ***/
 desc('Deploy your project');
-task('deploy', [
-    'deploy:prepare',
-    'deploy:lock',
-    'deploy:release',
-    'deploy:update_code',
-    'deploy:shared',
-    'deploy:writable',
-    'deploy:vendors',
-    'assets:install',
-    'assets:build',
-    'deploy:clear_paths',
-    'deploy:optimize',
-    'deploy:symlink',
-    'deploy:unlock',
-    'cleanup',
-    'success',
-]);
+task(
+    'deploy',
+    [
+        'deploy:prepare',
+        'deploy:lock',
+        'deploy:release',
+        'deploy:update_code',
+        'deploy:shared',
+        'deploy:writable',
+        'deploy:vendors',
+        'assets:install',
+        'assets:build',
+        'deploy:clear_paths',
+        'deploy:optimize',
+        'deploy:symlink',
+        'deploy:unlock',
+        'cleanup',
+        'success',
+    ]
+);
 
 /**
  * Helper tasks
  */
-desc('Optimize deployed application');
-task('deploy:optimize', ['bytic:optimize']);
-
-after('cleanup', 'assets:cleanup');
-
-task('assets:cleanup',  function () {
-    if (!has('previous_release')) {
-        return;
-    }
-    if (!test('[ -d {{previous_release}}/node_modules ]')) {
-        return;
-    }
-
-    $sudo = get('cleanup_use_sudo') ? 'sudo' : '';
-    run("$sudo rm -rf {{previous_release}}/node_modules");
-});
-
 before('deploy:update_code', 'deploy:git-cache');
 after('deploy:symlink', 'deploy:storage-symlink');
 after('deploy:failed', 'deploy:unlock');
+
+desc('Optimize deployed application');
+task('deploy:optimize', ['bytic:optimize']);
+
+after('cleanup', 'cleanup:previous_release');
+task(
+    'cleanup:previous_release',
+    [
+        'deploy:previous_release:cleanup_node',
+        'bytic:previous_release:cleanup_vendors',
+        'bytic:previous_release:cleanup_cache'
+    ]
+);
